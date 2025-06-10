ccb011fce2b6ae99ca76e61b9258e7d2b844b009
Roberds <robertodiazgs@gmail.com>
2025-06-10 11:35:42 +0200

    first commit

---
+++ src/test/java/com/robds/jsonplaceholder/api/JsonplaceholderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robds.jsonplaceholder.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonplaceholderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
